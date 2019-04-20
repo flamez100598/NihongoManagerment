f3f71427c4007a7288d8df0722438cac47d349b8
PhamQuocDung <mrvvip11@gmail.com>
2019-04-20 19:46:34 +0700

    commit UI and fix db

--- CentManagerment.Model/EF/CentManagerment.Designer.cs
+++ CentManagerment.Model/EF/CentManagerment.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\BACK-END\ASP.NET\Projects\3ChangNgoc\NihongoManagerment\CentManagerment.Model\EF\CentManagementDbContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\Desktop\NihongoManagerment\End\NihongoManagerment\CentManagerment.Model\EF\CentManagerment.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
